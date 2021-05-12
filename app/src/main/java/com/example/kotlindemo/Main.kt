1cf8f86ab45ad73eb812076aa78d9d807252209f
Badrichetan <badricheyan7@gmail.com>
2021-05-12 20:41:00 +0530

    App Area Of Circle

--- app/src/main/java/com/example/kotlindemo/Main.kt
+++ app/src/main/java/com/example/kotlindemo/Main.kt
@@ -1,10 +1,7 @@
 package com.example.kotlindemo
 
 fun  main() {
-    var myName:String = "Andriod Kotlin"
-    var myFloat:Float = 13.37F
-    var myByte=25
+    var a= 10
+    print(++a)
 
-    val myDouble=3.14159265358979
-    print("$myName\n$myFloat\n$myByte\n$myDouble ")
 }
--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,13 +1,37 @@
-package com.example.myapplication
+package com.example.kotlindemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
 import android.os.Bundle
-import com.example.kotlindemo.R
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.btn)
+        var txt = findViewById<TextView>(R.id.txt)
+        var input_result=findViewById<EditText>(R.id.input_radius)
+
+
+        btn.setOnClickListener(View.OnClickListener {
+
+            // Code here executes on main thread after user presses button
+            val pi:Double=3.141
+            var a:Editable = input_result.text
+            var b:String = a.toString()
+            var radius:Double = b.toDouble()
+
+           // var radius:Double= input_result.text.toString().toDouble()
+            var area =pi*radius*radius
+            txt.setText("Result : $area")
+        })
+
     }
 }
