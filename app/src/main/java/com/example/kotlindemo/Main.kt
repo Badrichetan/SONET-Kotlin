27bdbfa930d4ced50d8d952f7e73c1cbce1b8d81
Badrichetan <badricheyan7@gmail.com>
2021-05-14 18:12:38 +0530

    Spinner with Color:when

--- app/src/main/java/com/example/kotlindemo/Main.kt
+++ app/src/main/java/com/example/kotlindemo/Main.kt
@@ -1,7 +1,20 @@
 package com.example.kotlindemo
 
-fun  main() {
-    var a= 10
-    print(++a)
+fun  main(){
+    var n = 11
+    var res:String = if(n>10)"hello" else "world"
+
+    res = when(n)
+    {
+        11 ->{
+            "hello\n"+
+            "abc"
+        }
+        in 2..10 -> "World"
+        11,12,13 -> "Test"
+        else -> "no data"
+    }
+
+    print(res)
 
 }
