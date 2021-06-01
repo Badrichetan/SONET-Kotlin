eadfb26cfedc78164ed9f8f4dbc06ddad151adc4
Badrichetan <badricheyan7@gmail.com>
2021-06-01 19:30:24 +0530

    Loop example

--- app/src/main/java/com/example/kotlindemo/Main.kt
+++ app/src/main/java/com/example/kotlindemo/Main.kt
@@ -1,20 +1,15 @@
 package com.example.kotlindemo
 
 fun  main(){
-    var n = 11
-    var res:String = if(n>10)"hello" else "world"
+    var text:String = "Hello World"
+    var i:Int=0;
 
-    res = when(n)
-    {
-        11 ->{
-            "hello\n"+
-            "abc"
-        }
-        in 2..10 -> "World"
-        11,12,13 -> "Test"
-        else -> "no data"
+    for(l in text){
+        print("$l, ")
+        ++i;
     }
+    println("number of letters: $i")
 
-    print(res)
+
 
 }
