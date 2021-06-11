dc7b4421ad60766302ecb2523d47dcd273c9fead
Badrichetan <badricheyan7@gmail.com>
2021-06-11 19:26:15 +0530

    constructor

--- app/src/main/java/com/example/kotlindemo/Main.kt
+++ app/src/main/java/com/example/kotlindemo/Main.kt
@@ -1,2 +1,9 @@
 package com.example.kotlindemo
 
+fun main(){
+    var p1:Person = Person("SkR",220.0)
+    //println(p1.name)
+   // println(p1.weightKilo)
+  //  println(p1.eatSweet(false))
+   // println(p1.weightKilo)
+}
--- app/src/main/java/com/example/kotlindemo/Person.kt
+++ app/src/main/java/com/example/kotlindemo/Person.kt
@@ -1,14 +1,28 @@
 package com.example.kotlindemo
 
-class Person constructor(name:String, zz:Double){
-
-    val name:String = name
-    var weightlbs:Double = zz
-
-    var weightKilo:Double
-    get() = weightlbs/2.0
-    set(value){
-        weightlbs = value+2.0
+class Person(var name:String, var weightLbs:Double) {
+    init{
+        print("Test")
+    }
+    init{
+        println("Test1")
+    }
+    //Secodary constructor
+    constructor(name:String): this("",0.0 ){
+        println("Hello")
     }
 
+    var weightKilo:Double
+        get() = weightLbs/2.0
+        set(value) {
+            weightLbs =  value*2.0
+        }
+
+    fun eatSweet(addIcescream:Boolean){
+        weightLbs += if(addIcescream) 4.0 else 2.0
+    }
+
+    fun calGoal(weightToLoos:Double):Double{
+        return weightLbs - weightToLoos
+    }
 }
